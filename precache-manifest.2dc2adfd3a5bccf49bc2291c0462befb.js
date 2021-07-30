f89733a80ba989577e87fd331cd11171e059dd16
Olisaeloka Imonugo <ollimmo@gmail.com>
2021-07-30 18:48:02 -0400

    Updates

---
+++ precache-manifest.2dc2adfd3a5bccf49bc2291c0462befb.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04d845f5eaa84304ed9160f85fbccf7a",
+    "url": "/superreactport/index.html"
+  },
+  {
+    "revision": "797b78e77d7296bfb54e",
+    "url": "/superreactport/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "52ace55b4328be6e494b",
+    "url": "/superreactport/static/css/main.fdfc1996.chunk.css"
+  },
+  {
+    "revision": "797b78e77d7296bfb54e",
+    "url": "/superreactport/static/js/2.d7dc23ce.chunk.js"
+  },
+  {
+    "revision": "a1e12f97867665430e5c8e39fc9f180a",
+    "url": "/superreactport/static/js/2.d7dc23ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52ace55b4328be6e494b",
+    "url": "/superreactport/static/js/main.137773a4.chunk.js"
+  },
+  {
+    "revision": "e1e7a07f9d08278ef086",
+    "url": "/superreactport/static/js/runtime-main.36cd7256.js"
+  },
+  {
+    "revision": "581673f8cf03d2ebd28ebfb560a98650",
+    "url": "/superreactport/static/media/JavascriptQuiz.581673f8.png"
+  },
+  {
+    "revision": "463bf3769b5f0bcb70df19a029baba56",
+    "url": "/superreactport/static/media/NoteTaker.463bf376.png"
+  },
+  {
+    "revision": "8f81bd2bccab11e1af67553901ea42fb",
+    "url": "/superreactport/static/media/Project3ComingSoon.8f81bd2b.png"
+  },
+  {
+    "revision": "8aa343295420a4a72eb25a1a4106d2ff",
+    "url": "/superreactport/static/media/RefactorHomework.8aa34329.png"
+  },
+  {
+    "revision": "047433e759911fe97d3cd1284ca2f274",
+    "url": "/superreactport/static/media/TechBlog.047433e7.png"
+  },
+  {
+    "revision": "e297b0e0beed32664f5f3d0cae455f10",
+    "url": "/superreactport/static/media/TheWatchSensor.e297b0e0.png"
+  },
+  {
+    "revision": "e4c935adf891a2c7a2f99f390aee2710",
+    "url": "/superreactport/static/media/WorkoutTracker.e4c935ad.png"
+  },
+  {
+    "revision": "66687ff849b223affe254ce6a8b1fb4f",
+    "url": "/superreactport/static/media/jogjotter.66687ff8.png"
+  },
+  {
+    "revision": "38e24bd602a42abe72e8abf72e430592",
+    "url": "/superreactport/static/media/landing_bg.38e24bd6.jpg"
+  },
+  {
+    "revision": "925f48dfb313edf7f80de0b3c666ae80",
+    "url": "/superreactport/static/media/oli_profile.925f48df.JPG"
+  }
+]);
