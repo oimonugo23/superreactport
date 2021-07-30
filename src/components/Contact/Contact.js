a193a5f1f4dff8d2f8f12293c83af21f54d6f6b5
Olisaeloka Imonugo <ollimmo@gmail.com>
2021-07-30 18:47:37 -0400

    adding code for contact and education

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
+
+class Contact extends Component {
+    render() {
+        return (
+            <div className='contact-body'>
+                <Grid className='contact-grid'>
+                    <Cell col={6}>
+                        <h2>Oli Imonugo</h2>
+                        <img src='https://www.pngitem.com/pimgs/m/10-106042_male-professional-avatar-icon-hd-png-download.png'
+                            alt='avatar'
+                            style={{ height: '250px' }} />
+                    </Cell>
+                    <Cell col={6}>
+                        <h2>Contact Me</h2>
+                        <hr />
+                        <div className='contact-list'>
+                            <List>
+                                <ListItem>
+                                    <ListItemContent style={{ fontSize: '25px', fontFamily: 'Anton' }}>
+                                        <i className='fa fa-github-square' aria-hidden='true' />oimonugo23</ListItemContent>
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemContent style={{ fontSize: '25px', fontFamily: 'Anton' }}>
+                                        <i className='fa fa-envelope' aria-hidden='true' />ollimmo@gmail.com</ListItemContent>
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemContent style={{ fontSize: '25px', fontFamily: 'Anton' }}>
+                                        <i className='fa fa-twitter-square' aria-hidden='true' />ollimmo</ListItemContent>
+                                </ListItem>
+                            </List>
+                        </div>
+                    </Cell>
+                </Grid>
+            </div>
+        );
+    }
+}
+
+export default Contact;
